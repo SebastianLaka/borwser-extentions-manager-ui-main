134a353352735cfec207a9cede39a2c3e79766dd
Sebastian <sebastianlakla@gmail.com>
2025-04-13 10:57:27 +0200

    Created directory for icons and migrated icons

--- src/components/HeaderApp.vue
+++ src/components/HeaderApp.vue
@@ -1,20 +1,20 @@
 <script setup></script>
 <template>
   <div class="ui-header">
-    <img class="ui-header__logo" src="../assets/logo.svg" alt="Logo browser extentions UI" />
-    <button class="ui-header__btn"><img src="../assets/icon-moon.svg" alt="" /></button>
+    <img class="ui-header__logo" src="../assets/icons/logo.svg" alt="Logo browser extentions UI" />
+    <button class="ui-header__btn"><img src="../assets/icons/icon-moon.svg" alt="" /></button>
   </div>
 </template>
 <style scoped lang="scss">
 @import '../assets/colors.scss';
-/* - **Neutral 900**: `hsl(227, 75%, 14%)`
-- **Neutral 800**: `hsl(226, 25%, 17%)`
-- **Neutral 700**: `hsl(225, 23%, 24%)`
-- **Neutral 600**: `hsl(226, 11%, 37%)`
-- **Neutral 300**: `hsl(0, 0%, 78%)`
-- **Neutral 200**: `hsl(217, 61%, 90%)`
-- **Neutral 100**: `hsl(0, 0%, 93%)`
-- **Neutral 0**: `hsl(200, 60%, 99%)` */
+// /* - **Neutral 900**: `hsl(227, 75%, 14%)`
+// - **Neutral 800**: `hsl(226, 25%, 17%)`
+// - **Neutral 700**: `hsl(225, 23%, 24%)`
+// - **Neutral 600**: `hsl(226, 11%, 37%)`
+// - **Neutral 300**: `hsl(0, 0%, 78%)`
+// - **Neutral 200**: `hsl(217, 61%, 90%)`
+// - **Neutral 100**: `hsl(0, 0%, 93%)`
+// - **Neutral 0**: `hsl(200, 60%, 99%)` */
 $header-border-radious: 1em;
 .ui-header {
   display: flex;
