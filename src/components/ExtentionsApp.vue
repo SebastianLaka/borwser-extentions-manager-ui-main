3ef7f3d569def42771c4746632751ad73e03c907
Sebastian <sebastianlakla@gmail.com>
2025-04-15 18:51:44 +0200

    Imported all files from sass directory in assets

--- src/components/ExtentionsApp.vue
+++ src/components/ExtentionsApp.vue
@@ -3,4 +3,8 @@
 <template>
 </template>
 <style scoped lang="scss">
+@import '../assets/sass/colors.scss';
+@import '../assets/sass/border.scss';
+@import '../assets/sass/mixins.scss';
+@import '../assets/sass/fonts.scss';
 </style>
