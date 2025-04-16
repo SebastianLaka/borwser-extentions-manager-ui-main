e02a9091b67738b3a6834fc26e2272f7eb3a966f
Sebastian <sebastianlakla@gmail.com>
2025-04-16 19:03:21 +0200

    Add card structure in html

--- src/components/ExtentionsApp.vue
+++ src/components/ExtentionsApp.vue
@@ -10,18 +10,19 @@ function getLogoPath(path) {
 
 <template>
   <section class="extentions-container">
-    <div
-      class="extention"
-      v-for="(data, index) in dataJSON"
-      :key="index"
-    >
-    <img
-        :src="getLogoPath(data.logo)"
-        :alt="`${data.name}`"
-      />
-      <div class="extention-description">
-        <p class="extention-description__name">{{ data.name }}</p>
-        <p class="extention-description__about">{{ data.description }}</p>
+    <div class="extention" v-for="(data, index) in dataJSON" :key="index">
+      <div class="extention-card">
+        <div class="extention-image">
+          <img :src="getLogoPath(data.logo)" :alt="`${data.name}`" />
+        </div>
+        <div class="extention-description">
+          <p class="extention-description">{{ data.name }}</p>
+          <p class="extention-description">{{ data.description }}</p>
+        </div>
+      </div>
+      <div class="extention-button-area">
+        <button>Remove</button>
+        <button>XX</button>
       </div>
     </div>
   </section>
@@ -31,4 +32,19 @@ function getLogoPath(path) {
 @import '../assets/sass/border.scss';
 @import '../assets/sass/mixins.scss';
 @import '../assets/sass/fonts.scss';
+@media (min-width: 20em) {
+  .extentions-container {
+    .extention {
+      display: flex;
+      flex-direction: column;
+     .extention-card{
+      display: flex;
+     }
+      .extention-button-area {
+        display: flex;
+        justify-content: space-between;
+      }
+    }
+  }
+}
 </style>
