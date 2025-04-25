c395f5e3d499ebdc2e287f714f1445943f198db9
Sebastian <sebastianlakla@gmail.com>
2025-04-25 21:42:32 +0200

    Created dark mode at header and nav in app

--- src/App.vue
+++ src/App.vue
@@ -8,19 +8,22 @@ import AppMain from './components/AppMain.vue'
 
 <style lang="scss">
 @import './assets/sass/reset.scss';
-@import './assets/sass/colors.scss';
-@import './assets/sass/border.scss';
-@import './assets/sass/mixins.scss';
-html.dark {
-  body {
-    background: $dark-Gradient;
-    color: $neutral-0;
-  }
-  .ui-header{
-    background-color: $neutral-800;
-    border: none;
-  }
-}
+// @import './assets/sass/colors.scss';
+// @import './assets/sass/border.scss';
+// @import './assets/sass/mixins.scss';
+// html.dark {
+//   body {
+//     background: changeColor($dark-Gradient);
+//     color: changeColor($neutral-0);
+//   }
+//   .ui-header {
+//     background-color: changeColor($neutral-800);
+//     border: none;
+//   }
+//   .moon-img{
+//       stroke: changeColor($neutral-700);
+//     }
+// }
 body {
   padding: 0 0.5em;
   font-family: 'Noto sans', sans-serif;
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,19 +1,52 @@
 <script setup>
 import HeaderApp from './HeaderApp.vue'
 import NavApp from './NavApp.vue'
-import ExtentionsApp from './ExtentionsApp.vue';
+import ExtentionsApp from './ExtentionsApp.vue'
 </script>
 <template>
   <div class="wrapper">
-  <main class="app-container">
-    <HeaderApp />
-    <NavApp />
-    <ExtentionsApp/>
-  </main>
-</div>
+    <main class="app-container">
+      <HeaderApp />
+      <NavApp />
+      <ExtentionsApp />
+    </main>
+  </div>
 </template>
-<style scoped lang="scss">
-@import "../assets/sass/wrapper.scss";
+<style lang="scss">
+@import '../assets/sass/wrapper.scss';
+@import '../assets/sass/reset.scss';
+@import '../assets/sass/colors.scss';
+@import '../assets/sass/border.scss';
+@import '../assets/sass/mixins.scss';
+.dark {
+  body {
+    background: changeColor($dark-Gradient);
+    color: changeColor($neutral-0);
+
+    .ui-header {
+      background-color: changeColor($neutral-800);
+      border: none;
+      &__btn{
+        background-color: changeColor($neutral-800);
+      }
+      .sun-img{
+        color: changeColor($neutral-0);
+      }
+    }
+    .ui-nav {
+      .ui-nav-button {
+        &__all,
+        &__active,
+        &__inactive {
+          background-color: changeColor($neutral-800);
+          color: changeColor($neutral-0);
+          border: none;
+          border: 0.175em solid changeColor($neutral-700);
+        }
+      }
+    }
+  }
+}
 .app-container {
   display: flex;
   flex-direction: column;
